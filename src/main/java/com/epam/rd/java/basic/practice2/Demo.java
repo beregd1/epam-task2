f009fb562986f0033d6a5806c34d0000d8bf0d9b
Dimon <beregd2013@gmail.com>
2021-03-22 23:12:43 +0200

    test1

--- src/main/java/com/epam/rd/java/basic/practice2/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice2/Demo.java
@@ -3,6 +3,17 @@ package com.epam.rd.java.basic.practice2;
 public class Demo {
 
     public static void main(String[] args) {
-        
+        Array a = new ArrayImpl(1);
+        a.add('A');
+        a.add('B');
+        a.add('C');
+        System.out.println(a.size());
+        System.out.println(a);
+        a.set(2,'D');
+        System.out.println(a);
+        a.remove(1);
+        System.out.println(a);
+        System.out.println(a.indexOf('A'));
+        System.out.println(a.size());
     }
 }
